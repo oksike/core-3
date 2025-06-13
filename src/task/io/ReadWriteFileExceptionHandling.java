040841089371fd32c7cc5796d7471ffb575a8688
Oksana <oksana.larpunina.04@mail.ru>
2025-06-13 16:21:05 +0300

    Решение всех задач

--- src/task/io/ReadWriteFileExceptionHandling.java
+++ src/task/io/ReadWriteFileExceptionHandling.java
@@ -1,32 +1,93 @@
 package task.io;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
 public class ReadWriteFileExceptionHandling {
 
   public static void createAndWriteFile(String fileName, String content) {
+      try {
+        Path filePath = Paths.get(fileName);
+        Files.write(filePath, content.getBytes());
+      } catch (IOException e) {
+
+      }
   }
 
   public static void createFileFromConsoleInput() {
+    Scanner scanner = new Scanner(System.in);
+    String fileName = scanner.nextLine();
+    String content = scanner.nextLine();
+    try {
+      Path filePath = Paths.get(fileName);
+      Files.write(filePath, content.getBytes());
+    } catch (IOException e){
+
+    }
   }
 
   public static double divideNumbers(double a, double b) {
-    return 0;
+    if (b==0) {
+      throw new ArithmeticException("ошибка");
+    }
+    return a/b;
   }
 
   public static void readNumberAndPrintSquare() {
+    Scanner scanner = new Scanner(System.in);
+    Double number = scanner.nextDouble();
+    try {
+      double square = number*number;
+      System.out.println(square);
+    } catch (ArithmeticException e) {
+
+    }
+
   }
 
   public static void readFileAndPrint(String fileName) {
-
+    try {
+      Path filePath = Paths.get(fileName);
+      List<String> lines = Files.readAllLines(filePath);
+      for (String line : lines) {
+        System.out.println(line);}
+    } catch (IOException e){ }
   }
 
   public static int convertStringToNumber(String str) {
-    return 0;
+    try {
+      return Integer.parseInt(str);
+    } catch (NumberFormatException e){
+      throw new NumberFormatException("error");
+    }
+
   }
 
   public static void divideConsoleInput() {
+    Scanner scanner = new Scanner(System.in);
+    Double number1 = scanner.nextDouble();
+    Double number2 = scanner.nextDouble();
+    try {
+      double res = number1/number2;
+      System.out.println(res);
+    } catch (ArithmeticException e){}
+
   }
 
   public static void createFileAndReadFirstWord(String fileName, String content) {
+    try {
+      Path filePath = Paths.get(fileName);
+      Files.write(filePath, content.getBytes());
+      String fileContent = new String(Files.readAllBytes(filePath));
+      String[] words = fileContent.split("//");
+      System.out.println(words[0]);
+    } catch (IOException e) {
+
+    }
 
   }
 
--- src/task/stream/Stream.java
+++ src/task/stream/Stream.java
@@ -3,62 +3,91 @@ package task.stream;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Stream {
-//dlrjh
+//задание 1
   public static int addNumbers(int num1, int num2) {
-    return 0;
+    return num1+num2;
   }
-
+  //задание 2
   public static List<String> filterWordsByLetter(List<String> words, char letter) {
-    return null;
+    Predicate<String> predicate1 = firstL -> firstL.charAt(0) == letter;
+    return words.stream()
+            .filter(predicate1)
+            .collect(Collectors.toList());
   }
-
+  // задание 3
   public static List<Integer> squareNumbers(List<Integer> numbers) {
-    return null;
+    Function<Integer, Integer> squareF = x-> x*x;
+    return numbers.stream()
+            .map(squareF)
+            .collect(Collectors.toList());
   }
-
+  // задание 4
   public static void printUpperCase(List<String> words) {
+    Consumer<String> UppCase = wordUpp -> wordUpp.toUpperCase();
   }
-
+// задание 5
   public static int sumOfNumbers(List<Integer> numbers) {
-    return 0;
+    return numbers.stream()
+            .reduce(0,Integer::sum);
   }
-
+//задание 6
   public static OptionalDouble averageOfNumbers(List<Integer> numbers) {
-    return null;
+    return numbers.stream()
+            .mapToInt(Integer::intValue)
+            .average();
   }
 
   public static Optional<Integer> findMaxNumber(List<Integer> numbers) {
-    return null;
+    return numbers.stream()
+            .max(Integer::compareTo);
+
   }
 
   public static Optional<Integer> findMinNumber(List<Integer> numbers) {
-    return null;
+    return numbers.stream()
+            .min(Integer::compareTo);
   }
 
   public static boolean containsValue(List<Integer> numbers, int value) {
-    return false;
+    return numbers.stream()
+            .anyMatch(chekEl->chekEl.equals(value));
   }
 
   public static List<Integer> removeDuplicates(List<Integer> numbers) {
-    return null;
+    return numbers.stream()
+            .distinct()
+            .collect(Collectors.toList());
   }
 
   public static List<Integer> stringLengths(List<String> words) {
-    return null;
+    return words.stream()
+            .map(String::length)
+            .collect(Collectors.toList());
   }
 
   public static String concatenateStrings(List<String> words) {
-    return null;
+    return words.stream()
+            .map(String::trim)
+            .collect(Collectors.joining(" "));
+
   }
 
   public static List<Integer> filterEvenNumbers(List<Integer> numbers) {
-    return null;
+    return numbers.stream()
+            .filter(x->x%2==0)
+            .collect(Collectors.toList());
   }
 
   public static Optional<Integer> findFirstEvenNumber(List<Integer> numbers) {
-    return null;
+    return numbers.stream()
+            .filter(x->x%2==0)
+            .findFirst();
   }
 
 }
